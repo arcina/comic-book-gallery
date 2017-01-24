b2ffedaacf946b55c346bec84d16663c78514195
arcina <arciniegas.sin@gmail.com>
2017-01-23 21:01:47 -0600

    Update Comic Book Detail view to be strongly typed

--- src/ComickBookGallery/Models/ComicBook.cs
+++ src/ComickBookGallery/Models/ComicBook.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ComickBookGallery.Models
+namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
