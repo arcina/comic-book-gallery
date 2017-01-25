ba2adfd2684efbdc668c231fab473fefd15540bf
arcina <arciniegas.sin@gmail.com>
2017-01-25 14:05:40 -0600

    Added Comic Books List View

--- src/ComickBookGallery/Data/ComicBookRepository.cs
+++ src/ComickBookGallery/Data/ComicBookRepository.cs
@@ -60,6 +60,10 @@ namespace ComickBookGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks() {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id) {
 
             ComicBook comicBookToReturn = null;
